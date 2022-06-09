f12fe48dd18b5ed81df32308f28decca4cc1dde1
MRomero <romerolaguado@gmail.com>
2022-06-09 10:55:20 -0500

    Mi primer TS

---
+++ TypeScript/EjemploUno.ts
@@ -0,0 +1,14 @@
+function holaMundo(){
+    console.log("Hola mundo");
+}
+
+holaMundo();
+/* Se debe escribir en la terminal (cmd):
+tsc Ejemplo.ts
+--> con el fin de obtener un archivo tipo JS con el mismo nombre del
+archivo TS para poder ejecutarlo más tarde.
+-----
+En la terminal se digita:
+node EjemploUno.js
+--> para ejecutar el mensaje en la consola de holaMUndo()
+*/
