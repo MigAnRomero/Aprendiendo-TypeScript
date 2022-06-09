378681c8e5018314b742846fc11f721819f069ba
MRomero <romerolaguado@gmail.com>
2022-06-09 11:43:57 -0500

    Se ha habilitado la ejecución de scripts.
    Se ha creado el archivo tipo JS de EjemploUno.ts

---
+++ TypeScript/EjemploUno.js
@@ -0,0 +1,13 @@
+function holaMundo() {
+    console.log("Hola mundo");
+}
+holaMundo();
+/* Se debe escribir en la terminal (cmd):
+tsc Ejemplo.ts
+--> con el fin de obtener un archivo tipo JS con el mismo nombre del
+archivo TS para poder ejecutarlo más tarde.
+-----
+En la terminal se digita:
+node EjemploUno.js
+--> para ejecutar el mensaje en la consola de holaMUndo()
+*/
