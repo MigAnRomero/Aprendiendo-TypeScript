e351923702f6c2ec16f79220958e4ef6df2bc8da
Miguel Romero <romerolaguado@gmail.com>
2022-06-09 15:02:27 -0500

    Terminando con el primer ejercicio.

--- TypeScript/EjemploUno.js
+++ TypeScript/EjemploUno.js
@@ -1,7 +1,6 @@
 function holaMundo() {
     console.log("Hola mundo");
 }
-holaMundo();
 /* Se debe escribir en la terminal (cmd):
 tsc Ejemplo.ts
 --> con el fin de obtener un archivo tipo JS con el mismo nombre del
@@ -11,3 +10,8 @@ En la terminal se digita:
 node EjemploUno.js
 --> para ejecutar el mensaje en la consola de holaMUndo()
 */
+function multiplicar(x, y) {
+    return x * y;
+}
+holaMundo();
+console.log(multiplicar(6, 8));
--- TypeScript/EjemploUno.ts
+++ TypeScript/EjemploUno.ts
@@ -2,7 +2,6 @@ function holaMundo(){
     console.log("Hola mundo");
 }
 
-holaMundo();
 /* Se debe escribir en la terminal (cmd):
 tsc Ejemplo.ts
 --> con el fin de obtener un archivo tipo JS con el mismo nombre del
@@ -12,3 +11,10 @@ En la terminal se digita:
 node EjemploUno.js
 --> para ejecutar el mensaje en la consola de holaMUndo()
 */
+function multiplicar(x, y)
+{
+    return x*y;
+}
+
+holaMundo();
+console.log(multiplicar(6,8));
