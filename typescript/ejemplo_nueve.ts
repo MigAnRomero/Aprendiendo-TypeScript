6c662a648a0fe4567fd15de07de69153c8c4a6f0
Miguel Romero <romerolaguado@gmail.com>
2022-06-12 22:04:42 -0500

    Practicando con las sentencias IF, ELSE IF, ELSE y el uso del operador ternario.

---
+++ typescript/ejemplo_nueve.ts
@@ -0,0 +1,51 @@
+//IF ELSE
+// console.log("Condición = true");
+// let condicion = true;
+// if(condicion)
+// {
+//     console.log("Este se ejecutara siempre");
+//     console.log("Este se ejecutara siempre");
+//     console.log("Este se ejecutara siempre");
+// }
+
+// console.log("Condición = false");
+// condicion = false;
+// if(condicion)
+// {
+//     console.log("Este se ejecutara siempre");
+//     console.log("Este se ejecutara siempre");
+//     console.log("Este se ejecutara siempre");
+// }
+// else
+// {
+//     console.log("No pasa nada bruh!");
+// }
+
+// Condiciones con números
+let x: number = 5;
+let y:number = 11;
+
+// if((x + y) > 15)
+// {
+//     console.log("La suma es mayor que 15");
+// }
+// else if((x + y) == 10)
+// {
+//     console.log("La suma es igual a 10");
+// }
+// else if((x + y) == 11)
+// {
+//     console.log("La suma es igual a 11");
+// }
+// else if((x + y) == 12)
+// {
+//     console.log("La suma es igual a 12");
+// }
+// else
+// {
+//     console.log("La suma esta entre 12 y 15 o es menor que 10")
+// }
+
+//Operador Ternario
+//Condicion ? true: false
+x > y ? console.log("x es mayor que y"): console.log("x es menor que y");
