4e3f6a8cfc084f97289a2315f8172a2776a82938
Miguel Romero <romerolaguado@gmail.com>
2022-06-13 14:41:07 -0500

    Practicando ciclo while y do-while

---
+++ typescript/ejemplo_doce.ts
@@ -0,0 +1,15 @@
+//Bucle While 
+// let num: number = 3;
+// while (num < 57 && num % 2 == 1)
+// {
+//     console.log("El número es: " + num);
+//     num *= 2;
+// }
+
+//Bucle Do-While
+let i: number = 5;
+do
+{
+    console.log("El número es " + i);
+    i++;
+}while(i < 7)
