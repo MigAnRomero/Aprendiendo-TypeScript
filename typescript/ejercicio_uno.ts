23767c87ec029c4bf72abc4b717bcc60b17c29ed
MRomero <romerolaguado@gmail.com>
2022-06-14 11:42:30 -0500

    Practicando con ejercicios lo aprendido.
    Todavía falta terminar de digitar el programa del archivo.

---
+++ typescript/ejercicio_uno.ts
@@ -0,0 +1,16 @@
+//1) Hacer la serie de fibonacci en TS
+
+let vecesIterar = 15;
+let numeros = [0, 1];
+let i = 2;
+
+while(i < vecesIterar)
+{
+    numeros[i] = numeros[i-2] + numeros[i-1];
+    i++;
+}
+console.log(numeros);
+console.log(numeros.length);
+
+//2) Devolver los primeros 100 números impares
+//3) Hacer un menú que nos devuelva una opción u otra.
