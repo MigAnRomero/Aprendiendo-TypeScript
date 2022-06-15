0be5d197b0ae1380f3c22842bbf128c3aa302f28
MRomero <romerolaguado@gmail.com>
2022-06-15 11:53:44 -0500

    Practicando las funciones en TS

---
+++ typescript/funcion_uno.ts
@@ -0,0 +1,18 @@
+function sumar(x: number, y: number): number
+{
+    return (x + y);
+}
+
+function sinRetornoNiParametros(x: string)
+{
+    console.log(x);
+}
+
+function mensajePrueba()
+{
+    console.log("Mensaje de prueba");
+}
+
+// console.log(sumar(2, 1));
+// console.log(sinRetornoNiParametros("Hola mundo"));
+console.log(mensajePrueba());
